b6ba78c34ce94452673190fcb4ae2e54268fbbd5
LAPTOP-COK9CFVT\ssma1 <ssma100701@gmail.com>
2018-12-05 21:40:51 +1100

    image upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.1/css/foundation.css">
 </head>
 <body>
+    @include('layouts.topbar')
+    <br>
     <div class="row">
+        @include('layouts.messages')
         @yield('content')
     </div>
 </body>
