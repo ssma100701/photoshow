95755d26053329a23d229717e09812426238f707
LAPTOP-COK9CFVT\ssma1 <ssma100701@gmail.com>
2018-12-05 23:16:59 +1100

    photo display

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -5,4 +5,31 @@
     <a href="/" class="button secondary">Go Back</a>
     <a href="/photos/create/{{ $album->id }}" class="button">Upload Photo To Album</a>
     <hr>
+    @if (count($album->photos) > 0)
+    <?php
+        $colcount = count($album->photos);
+        $i = 1;
+    ?>
+    <div id="photos">
+        <div class="row text-center">
+            @foreach ($album->photos as $photo)
+                <div class="columns medium-4 {{ $i == $colcount ? 'end': ''}}">
+                    <a href="/photos/{{ $photo->id }}">
+                        <img src="/storage/photos/{{ $photo->album_id }}/{{ $photo->photo }}" alt="{{ $photo->title }}" class="thumbnail">
+                    </a>
+                    <br>
+                    <h4>{{ $photo->title }}</h4>
+
+                @if ($i % 3 == 0)
+                    </div></div><div class="row text-center">
+                @else
+                    </div>
+                @endif
+                <?php $i++; ?>
+            @endforeach
+        </div>
+    </div>
+    @else
+        <p>No Photos To Display</p>
+    @endif
 @endsection
